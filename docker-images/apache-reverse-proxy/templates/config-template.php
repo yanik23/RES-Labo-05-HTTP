7560f874126485171182fd74a4ee4ab7955f43e4
yanik23 <ylange@windowslive.com>
2021-05-28 13:58:55 +0200

    Adding Load Balancer in php template

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,13 +1,38 @@
 <?php
-	$dynamic_app = getenv('DYNAMIC_APP');
-	$static_app = getenv('STATIC_APP');
+
+	$dynamic_app = explode(',', getenv('DYNAMIC_APP'));
+	$static_app = explode(',', getenv('STATIC_APP'));
+	
+	//$dynamic_app = getenv('DYNAMIC_APP');
+	//$static_app = getenv('STATIC_APP');
 ?>
 <VirtualHost *:80>
 	ServerName demo.res.ch
 	
-	ProxyPass '/api/students/' 'http://<?php print "$dynamic_app"?>/'
-	ProxyPassReverse '/api/students/' 'http://<?php print "$dynamic_app"?>/'
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
 	
-	ProxyPass '/' 'http://<?php print "$static_app"?>/'
-	ProxyPassReverse '/' 'http://<?php print "$static_app"?>/'	
+	<Proxy balancer://dynamicBalancer>
+<?php
+	foreach ($dynamic_app as &$dynamicIp){
+		echo "	BalancerMember http://". $dynamicIp . "\n";
+	}
+?>
+	</Proxy>
+	
+	<Proxy balancer://staticBalancer>
+<?php
+	foreach ($static_app as &$staticIp){
+		echo "	BalancerMember http://". $staticIp . "\n";
+	}
+?>
+	</Proxy>
+	
+	
+	ProxyPass '/api/students/' 'balancer://dynamicBalancer/'
+	ProxyPassReverse '/api/students/' 'balancer://dynamicBalancer/'
+	
+	ProxyPass '/' 'balancer://staticBalancer/'
+	ProxyPassReverse '/' 'balancer://staticBalancer/'
+	
 </VirtualHost>
